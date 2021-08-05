f72d02bd8ace2a9734e643aede1745858199d4e7
Jacob Loden <jacobloden@hotmail.com>
2021-08-05 16:50:49 -0700

    initial code push

---
+++ not_napping.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+import datetime
+import argparse
+
+def main():
+	''' TODO main stuff '''
+	i = 0
+
+if __name__ == "__main__":
+	main()
